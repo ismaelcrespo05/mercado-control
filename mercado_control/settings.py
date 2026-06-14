70f7cb17f3fdac6832d19699434ca288531f9404
ismaelcp <ismaelcrespo404@gmail.com>
2026-06-14 10:01:38 -0300

    fix whitenoise

--- mercado_control/settings.py
+++ mercado_control/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseSilencer',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
